fc81152fc405b6b076c9df2188f39ecb521878ab
Edo <Edo@naver.com>
2025-12-15 03:52:38 +0900

    Updates

---
+++ precache-manifest.972e1c76557bb7e72a22d2b222ba695d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f871f24030ff7da82b6afa96350caf6a",
+    "url": "/ninja-framer-motion/index.html"
+  },
+  {
+    "revision": "7a0471c89abd5946f9df",
+    "url": "/ninja-framer-motion/static/css/main.bf692695.chunk.css"
+  },
+  {
+    "revision": "d1cba8d67ed5dfe2acaa",
+    "url": "/ninja-framer-motion/static/js/2.0e4ccb7b.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/ninja-framer-motion/static/js/2.0e4ccb7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0471c89abd5946f9df",
+    "url": "/ninja-framer-motion/static/js/main.20fde21d.chunk.js"
+  },
+  {
+    "revision": "485a27e77d4f278ac084",
+    "url": "/ninja-framer-motion/static/js/runtime-main.980e54da.js"
+  }
+]);
