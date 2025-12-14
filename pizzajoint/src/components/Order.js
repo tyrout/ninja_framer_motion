a68d7aa734a05cb6c2c5034cf08293d96400c588
Edo <Edo@naver.com>
2025-12-15 01:15:16 +0900

    lesson 11 AnimatePresence 기초 1

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 
 const containerVariants = {
@@ -30,13 +30,24 @@ const childVariants = {
 }
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true);
+  setTimeout(() => setShowTitle(false), 4000);
+
   return (
     <motion.div className="container order"
       variants={containerVariants}
       initial="hidden"
       animate="visible"
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence> 
+        { showTitle &&  (
+          <motion.h2
+            exit={{ y: "-100vh", opacity:0}}
+          >
+            Thank you for your order :)</motion.h2>
+        )}
+      </AnimatePresence>
+
       <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
      
       {pizza.toppings.map(topping => 
