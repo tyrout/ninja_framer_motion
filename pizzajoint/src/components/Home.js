f4988fcf9e49705f7b59813fdabac41754421890
Edo <Edo@naver.com>
2025-12-15 01:03:19 +0900

    lesson-10 keyframe and yoyo

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+
+const buttonVariants = {
+  visible : {
+    // keyframe
+    x : [0, -20, 20, 0],
+    transition: { duration: 0.8 }
+  },
+  hover: {
+    scale: 1.1,
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: { yoyo: Infinity, duration: 0.3 }
+  } 
+}
+
+
 const Home = () => {
   return (
     <motion.div className="home container"
@@ -16,9 +32,9 @@ const Home = () => {
       </motion.h2>
       <Link to="/base">
         <motion.button
-          whileHover={{ scale: 1.1, 
-          boxShadow: "0px 0px 8px rgb(255,255,255)",
-          textShadow: "0px 0px 8px rgb(255,255,255)" }}
+          variants={buttonVariants}
+          whileHover="hover"
+          
         >
           Create Your Pizza
         </motion.button>
